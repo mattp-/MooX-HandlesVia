89ec1f3ad889db573f4735738d90876529912732
Matt Phillips <mphillips@oanda.com>
2012-12-19 09:46:06 -0500

    code cleanup, dependency removal (thanks tobyink)

--- lib/MooX/HandlesVia.pm
+++ lib/MooX/HandlesVia.pm
@@ -2,8 +2,6 @@ package MooX::HandlesVia;
 
 use strictures 1;
 use Module::Runtime qw/require_module/;
-use Package::Stash;
-use Syntax::Keyword::Junction qw/any/;
 
 sub import {
   my ($class) = @_;
@@ -12,15 +10,15 @@ sub import {
   no warnings 'redefine';
 
   my $target = caller;
-  my $has = $target->can('has');
-
-  *{$target.'::has'} = sub {
-    _process_has(@_);
-    $has->(@_);
+  if(my $has = $target->can('has')) {
+    *{$target.'::has'} = sub {
+      _process_has(@_);
+      $has->(@_);
+    }
   }
 }
 
-sub _process_has {
+sub process_has {
   my $name = shift;
   my %opts = @_;
   my $handles = $opts{handles} || return;
@@ -29,13 +27,9 @@ sub _process_has {
     # try to load
     require_module($via);
 
-    # we were successful, get a list of provided methods
-    my @symbols = Package::Stash->new($via)->list_all_symbols('CODE');
-
     # we need to switch up handles refs that match @symbols
     while (my ($target, $method) = each %$handles) {
-      if (any(@symbols) eq $method) {
-        my $g = '${\\'.$via.'->can("'.$method.'")}';
+      if ($via->can($method)) {
         $handles->{$target} = '${\\'.$via.'->can("'.$method.'")}';
       }
     }
