4de3d87f848ecec9de00a6a903d9a432d30df2ea
Matt Phillips <mphillips@oanda.com>
2012-12-21 16:26:29 -0500

    adds helper function

--- lib/Data/Perl/Counter.pm
+++ lib/Data/Perl/Counter.pm
@@ -1,11 +1,19 @@
 package Data::Perl::Counter;
 
+require Exporter;
+
+BEGIN { @ISA = qw(Exporter) }
+
+@EXPORT = qw(counter);
+
 sub new { my $cl = shift; bless \$_[0], $cl }
 
-sub inc { $$_[0] =+ ($_[1] ? $_[1] : 1) }
+sub counter { Data::Perl::Counter->new(shift||0) }
 
-sub dec { $$_[0] =- ($_[1] ? $_[1] : 1) }
+sub inc { ${$_[0]} =+ ($_[1] ? $_[1] : 1) }
 
-sub reset { $$_[0] = 0 }
+sub dec { ${$_[0]} =- ($_[1] ? $_[1] : 1) }
+
+sub reset { ${$_[0]} = 0 }
 
 1;
